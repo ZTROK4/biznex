4c1bff1652a85edfc79e32727d60553511b225bb
ZTROK4 <blessonveenus2004@gmail.com>
2025-04-05 21:49:57 +0530

    commit 42

--- finance.js
+++ finance.js
@@ -245,7 +245,7 @@ router.get('/accounts', async (req, res) => {
 router.post('/add-man-transaction', async (req, res) => {
     try {
         const { type , description, amount, date } = req.body;
-        console.data(req.body);
+        console.log(req.body);
 
         if (!['income', 'expense'].includes(type)) {
             return res.status(400).json({ error: 'Invalid type. Use "income" or "expense".' });
