83aceea993f8769a4796127b6984da00b01f04f9
ZTROK4 <blessonveenus2004@gmail.com>
2025-04-08 00:33:04 +0530

    commit 42

--- job_list.js
+++ job_list.js
@@ -6,7 +6,6 @@ const jwt = require('jsonwebtoken');
 
 
 router.use(async (req, res, next) => {
-    console.log(req.headers);
     const token = req.headers['authorization']?.split(' ')[1];
     if (!token) {
         return res.status(400).json({ error: 'No token provided. Please log in first.' });
@@ -125,7 +124,7 @@ router.post('/delete_job', async (req, res) => {
 router.get('/get_jobs_with_applicants', async (req, res) => {
     try {
         const client_id = req.client_id;
-
+        console.log("cl:",client_id);
         if (!client_id) {
             return res.status(401).json({ error: "Unauthorized: User not logged in" });
         }
