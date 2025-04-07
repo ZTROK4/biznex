ead415b30aff34b8b5bc35f0ea1ee6d866e13c58
ZTROK4 <blessonveenus2004@gmail.com>
2025-04-08 00:22:46 +0530

    commit 42

--- job_list.js
+++ job_list.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken');
 
 
 router.use(async (req, res, next) => {
+    console.log(req.headers);
     const token = req.headers['authorization']?.split(' ')[1];
     if (!token) {
         return res.status(400).json({ error: 'No token provided. Please log in first.' });
