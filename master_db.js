410cdb77a060eb6f261901b6b7d3430e7f2265e6
ZTROK4 <blessonveenus2004@gmail.com>
2025-09-07 01:02:35 +0530

    cahneg db data in masterdb file

--- master_db.js
+++ master_db.js
@@ -2,9 +2,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
-  host: '65.0.19.183',
+  host: '13.201.59.200',
   database: 'master_db',
-  password: 'MiniProBizNex@%32,8t%O1g-t@DontHackMe',
+  password: 'aCas()0aDskldnkn12124',
   port: 5432,
 });
 
