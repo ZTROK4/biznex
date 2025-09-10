5786794a1bc1588afdc4481f5a2667f319266674
ZTROK4 <blessonveenus2004@gmail.com>
2025-09-11 00:11:52 +0530

    ecomproducts

--- subdomain.js
+++ subdomain.js
@@ -62,6 +62,33 @@ router.use(async (req, res, next) => {
     }
 });
 
+router.use(async (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+    if (!token) {
+        return res.status(400).json({ error: 'No token provided. Please log in first.' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        let user_id = decoded.id;
+   
+        user_id=parseInt(user_id);
+        const result = await req.db.query('SELECT user_id FROM users WHERE user_id = $1', [user_id]);
+
+        if (result.rows.length === 0) {
+
+            return res.status(401).json({ error: 'Invalid client ID. Unauthorized access.' });
+        }
+
+        req.user_id = user_id;
+
+        next();
+    } catch (error) {
+        console.error("JWT verification error:", error);
+        return res.status(401).json({ error: 'Invalid or expired token' });
+    }
+});
+
 
 
 
